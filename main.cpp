e755f4c702bdff765e288efd19eaa75ba3791ab2
sepehr <sepehrmsm1379@gmail.com>
2021-02-28 10:17:29 +0330

    simple oop in c++

--- main.cpp
+++ main.cpp
@@ -1,7 +1,69 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
+// simple oop in c++
+
+enum Gender{
+	MALE, FEMALE
+};
+
+class Person{
+private:
+	string name;
+	string lastName;
+	int age;
+	Gender gender;
+	
+public:	
+	Person(string nameInput, string lastNameInput, int ageInput, Gender genderInput){
+		name = nameInput;
+		lastName = lastNameInput;
+		age = ageInput;
+		gender = genderInput;
+	}
+	
+	// setters
+	void setName(string variable){
+		name = variable;
+	}
+	
+	void setLastName(string variable){
+		lastName = variable;
+	}
+	
+	void setAge(int variable){
+		age = variable;
+	}
+	
+	void setGender(Gender variable){
+		gender = variable;
+	}
+	
+	// getters
+	string getName(){
+		return name;
+	}
+	
+	string getLastName(){
+		return lastName;
+	}
+	
+	int getAge(){
+		return age;
+	}
+	
+	Gender getGender(){
+		return gender;
+	}
+	
+};
+
 int main(){
-	cout << "Hello world";
+	
+	Person person = Person("sepehr", "mollaei", 20, MALE);
+	
+	cout << person.getName() << " " << person.getLastName() << endl; 
+	
 }
